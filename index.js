d07dcb56160b4147a081f0ee128af9710d602f2e
Mohammed waseem tabrez <109520003+Alitabrez786123@users.noreply.github.com>
2025-05-05 22:51:58 -0400

    index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.post('/ocr', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
